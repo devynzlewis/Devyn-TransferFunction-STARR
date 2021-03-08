5bb5dd92dcdcd4ff1160444d54a201e9157b8973
devynzlewis <73604162+devynzlewis@users.noreply.github.com>
2021-03-08 14:57:18 -0500

    Update RISQ_MODEL.py
    Removed Old functions and old comments to increase readability

--- RISQ_MODEL.py
+++ RISQ_MODEL.py
@@ -1,107 +1,6 @@
-from random import seed
-from random import random
-seed(1)
-
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
-import tensorflow.keras.backend as K
 
-
-def my_loss_fn(y_true, y_pred):
-    temp = tf.py_function(func=approx, inp=[y_pred], Tout=tf.float32)
-
-    squared_difference = tf.square(y_true - temp)
-    print(temp)
-    return tf.reduce_mean(squared_difference, axis=-1)
-
-def works(x):
-    y = approx(x)
-    return y
-
-def Single_RISQ_CALC(theta_b,theta_t,theta_ap,eta_sqrd,tau_sqrd,tau_ap):
-    U_f = np.zeros((2, 2), dtype=complex)
-
-    S_BS1 = generate_S_DB(theta_b, theta_t, eta_sqrd, tau_sqrd)
-    S_BS2 = generate_S_DB(theta_b, theta_t, eta_sqrd, tau_sqrd)
-    S_phase_phi = generate_S_AP(theta_ap, tau_ap)
-    S_phase_theta = generate_S_AP(0, tau_ap)
-
-    A = S_BS1.dot(S_phase_phi)
-    B = A.dot(S_BS2)
-    U_f[:, :] = B.dot(S_phase_theta)
-
-    return U_f
-
-
-def Practice_Single_RISQ_CALC(theta_ap):
-    tau_sqrd = 1 / 2
-    eta_sqrd = 1 / 2
-    theta_t = np.pi / 3
-    theta_b = np.pi / 3
-    tau_ap = 1 / 2
-
-    U_f = np.zeros((2, 2), dtype=complex)
-
-    S_BS = generate_S_DB(theta_b, theta_t, eta_sqrd, tau_sqrd)
-    S_phase_phi = generate_S_AP(theta_ap, tau_ap)
-    S_phase_theta = generate_S_AP(0, tau_ap)
-
-    A = S_BS.dot(S_phase_phi)
-    B = A.dot(S_BS)
-    U_f[:, :] = B.dot(S_phase_theta)
-
-    out = abs((U_f[0][0]) ** 2)
-
-    out = np.absolute(out)
-
-    return out
-
-
-def practice_loss(angle_out):
-    temp = Practice_Single_RISQ_CALC(angle_out)
-    return temp - 0.5
-
-
-
-def hadamard_loss(P_a_c_Single):
-    samples = np.zeros(100)
-    for i in range(0,100):
-        rand = np.random.uniform(0,1)
-        if rand > P_a_c_Single:
-            samples[i] = 0
-        else:
-            samples[i] = 1
-    out = np.mean(samples)
-    print("Expectation value is: ", out)
-
-    loss = out - 0.5
-    print("Loss value is: ", loss)
-    return out
-
-def new_hadamard_loss(theta):
-    y = approx(theta)
-    # count = 0
-    # for i in range(0, 100):
-    #     rand = random()
-    # if rand > y:
-    #     count = count
-    # else:
-    #     count = count + 1.0
-    return y
-def createTFModel():
-    model = tf.keras.Sequential([
-        tf.keras.Input(shape=(1,), dtype=tf.dtypes.float32, name='commands_input'),
-        tf.keras.layers.Dense(10, activation='elu'),
-        tf.keras.layers.Dense(1)
-
-    ])
-
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.05)
-    loss = my_loss_fn
-    model.compile(optimizer=optimizer, loss=loss)
-
-    return model
 
 # ________________________________________________________________________________________
 
@@ -109,18 +8,17 @@ def createTFModel():
 def RISQ_Calc(theta_b,theta_t,theta_ap,eta_sqrd,tau_sqrd,tau_ap):
     N = len(theta_ap)
     U_f = np.zeros((2, 2, 5000), dtype=complex)
+    zeroangle = np.zeros(5000)
 
     for i in range(0, N):
-        S_BS1 = generate_S_DB(np.pi/3, np.pi/3, eta_sqrd, tau_sqrd)
-        S_BS2 = generate_S_DB(np.pi/3, np.pi/3, eta_sqrd, tau_sqrd)
+        S_BS1 = generate_S_DB(theta_b, theta_t, eta_sqrd, tau_sqrd)
+        S_BS2 = generate_S_DB(theta_b, theta_t, eta_sqrd, tau_sqrd)
         S_phase_phi = generate_S_AP(theta_ap[i], tau_ap)
-        # S_phase_theta = generate_S_AP(0, tau_ap)
+        S_phase_theta = generate_S_AP(zeroangle[i], tau_ap)
 
-        # # U_f[:, :, i] = S_BS * S_phase_phi * S_BS * S_phase_theta
-        # A = S_BS.dot(S_phase_phi)
-        # B = A.dot(S_BS)
         A = S_BS1.dot(S_phase_phi)
-        U_f[:, :, i] = A.dot(S_BS2)
+        B = A.dot(S_BS2)
+        U_f[:, :, i] = B.dot(S_phase_theta)
 
 
 
@@ -138,7 +36,6 @@ def generate_S_DB(theta_b, theta_t, eta_sqrd, tau_sqrd):
     T_t = generate_transfer(generate_S_t(theta_t, tau, kappa))
     T_I = generate_transfer(generate_S_I(eta, gamma))
 
-    # S_DB = T_b * T_I * T_t
     A = T_b.dot(T_I)
     S_DB = A.dot(T_t)
 
@@ -217,27 +114,23 @@ def generate_S_AP(theta, tau):
     return np.array([[1, 0], [0, a_c]])
 
 
-def plotRISQCurve(theta_ap, P_a_c, P_a_l, P_a_c_Single, dashedLine):
+def plotRISQCurve(theta_ap, P_a_c, P_a_l, dashedLine):
     plt.figure(1)
     plt.plot(theta_ap, P_a_c, "-b", label="|\u03B1|^2")
-    # plt.plot(theta_single, P_a_c_Single, "o")
-    # plt.plot(theta_ap, approx(theta_ap), '-g')
+
     plt.plot(theta_ap, P_a_l, "-g", label="|\u03B2|^2")
+
     plt.plot(theta_ap, dashedLine, 'r--')
+
     plt.legend(loc="upper right")
     plt.title("Output State Probabilities for a |0> Input State")
     plt.xlabel("All-Pass Ring Resonator Detuning \u03A6")
     plt.ylabel("Probability of Photon Measurement")
 
-    # plt.figure(2)
-    # plt.plot(theta_ap, phase_P_a_c, "-y")
     plt.show()
 
 
-def approx(x):
-    a = 0.59
-    y = 1.45 * (1 / (a * np.sqrt(2 * np.pi))) * K.exp((-1) * ((x)**2 / (2 * a**2)))
-    return y
+
 
 
 
@@ -245,31 +138,17 @@ def approx(x):
 #-------------------------------------RISQ_MODEL.m---------------------------------------------------------------------------------------------------------
 # Initialize parameters
 
-dummyU = np.zeros((2,2,5000))
-
 tau_stacked = 1/2
 eta_stacked = 1/2
 theta_t = np.pi/3
 theta_b = np.pi/3
 tau_ap = 1/2
 theta_ap = np.linspace(-np.pi, np.pi, 5000)
-print("Please enter an offset angle:")
-theta_single = 1.1
-theta_single = float(theta_single)
-
-N = len(dummyU[0][0][:])
-
-dummyU[0][0][:] = np.sin(theta_ap)
-dummyU[0][1][:] = np.cos(theta_ap)
-
-dashedLine = np.zeros(5000)
-dashedLine[:] = 0.5
-print(N)
+N = 5000
+dashedLine = np.ones(5000) * 0.5
 
 U_RISQ = RISQ_Calc(theta_b, theta_t, theta_ap, eta_stacked, tau_stacked, tau_ap)
 
-Single_U = Single_RISQ_CALC(theta_b, theta_t, theta_single, eta_stacked, tau_stacked, tau_ap)
-#U_RISQ = dummyU
 
 
 #%% ____________________________________________Output from mode a input (i.e., |0>)__________________________________________________________________________
@@ -277,9 +156,6 @@ Single_U = Single_RISQ_CALC(theta_b, theta_t, theta_single, eta_stacked, tau_sta
 P_a_c = abs((U_RISQ[0][0][:])**2)
 P_a_l = abs((U_RISQ[0][1][:])**2)
 
-P_a_c_Single = abs((Single_U[0][0])**2)
-print(P_a_c_Single)
-hadamard_loss(P_a_c_Single)
 # definining phase function from mode a to c for a single photon input
 
 phase_P_a_c = np.reshape(np.angle(U_RISQ[0, 0, :]), [N,])
@@ -289,15 +165,7 @@ phase_P_a_c = - np.unwrap(np.degrees(phase_P_a_c))
 phase_P_a_l = np.reshape(np.angle(U_RISQ[0, 1, :]), [N,])
 phase_P_a_l = np.degrees(phase_P_a_l)
 
-
-# _____________________________________________________________________________________
-model = createTFModel()
-
-commands = np.array([[0]], dtype=np.float32)
-
-expected_outputs = np.array([[0.5]], dtype=np.float32)
-
-plotRISQCurve(theta_ap, P_a_c, P_a_l, P_a_c_Single, dashedLine)
+plotRISQCurve(theta_ap, P_a_c, P_a_l, dashedLine)
 
 # ______________________________________________________________________________________
 
